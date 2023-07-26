162ce56f5c695632cfea940b07c1519c7363e5df
treboldeorion <132966239+treboldeorion@users.noreply.github.com>
2023-07-25 22:35:19 -0500

    cajero3

--- cajero.js
+++ cajero.js
@@ -6,10 +6,39 @@ var cuenta = [
 {nombre:"Jero",saldo:200, password:"5678"}
 ];
 
+/*VALIDAR CONTRASEÑA*/
+document.querySelector(".contenido").style.display = "none"  /*BORRA DISPLAY*/
+
+
+
+
+function enviar(){
+    let indice = parseInt(document.querySelector("#cuenta").value)
+    let pass1 = document.querySelector("#pass").value; /*CONTRASEÑA INGRESADA*/
+    let pass2 = cuenta [indice-1].password;   /*CONTRASEÑA GRABADA USUARIO*/
+    if(pass1 === pass2){
+    document.querySelector("#bienvenido").innerHTML="Bienvenido al Banco Caiman"
+    document.querySelector(".contenido").style.display= "block"
+    document.querySelector(".inicio").style.display = "none"
+    alert("Bienvenido")
+   }else
+   {document.querySelector("#bienvenido").innerHTML="Contraseña Invalida"
+   alert("Incorrecto")
+
+}
+   
+   
+}
+
+
+function cierre() {
+    window.location.reload()
+  }
+
+
 
 /*Como regla de negocio, una cuenta no debe de tener más de $990 y menos de $10. Es necesario hacer las validaciones pertinentes para que no se rompa esta regla de negocio.*/
-
-/*REGLA/**/
+/*REGLA*/
     
 function regla (){
     let indice = parseInt(document.querySelector("#cuenta").value)
@@ -20,40 +49,26 @@ function regla (){
     }
     if( saldo <10 ) {
         document.getElementById("bienvenido").innerHTML="Saldo insuficiente"
-    } else (document.getElementById("bienvenido").innerHTML="Realice sus transacciones con precaucion")
+    } else (document.getElementById("bienvenido").innerHTML="Transaccion Exitosa")
 }
 
-
-
-
-/*VALIDAR CONTRASEÑA*/
-
-function pas(){
-    let indice = parseInt(document.querySelector("#cuenta").value)
-    let pass1 = document.getElementById("pass").value; /*CONTRASEÑA INGRESADA*/
-    let pass2 = cuenta [indice-1].password;   /*CONTRASEÑA GRABADA USUARIO*/
-    if(pass1 === pass2){
-    document.getElementById("bienvenido").innerHTML="contraseña correcta, continue"
-   }
-}
 /*let msn = document.getElementById("passwordHelpBlock").value;*/
 
 
 
 document.querySelector("#cuenta").addEventListener("change",leerPersona)
-document.querySelector("#consig").addEventListener("click",leerconsignar)
-document.querySelector("#reti").addEventListener("click",leerretirar)
-document.querySelector("#bienvenido").addEventListener("click",pas)
+document.querySelector("#consig").addEventListener("click",leerconsignar,regla)
+document.querySelector("#reti").addEventListener("click",leerretirar,regla)
+
 
 /*LEER PERSONSA*/
 
 function leerPersona(){
     let indice = parseInt(document.querySelector("#cuenta").value);
-    console.log(cuenta[indice-1]);
     let cliente = cuenta[indice-1].nombre;
     let saldo = cuenta [indice-1].saldo;
-    document.getElementById("titulo").innerHTML = "Hola," + cliente;
-    document.getElementById("descripcion").innerHTML = "Tu saldo es ,$ " + saldo;
+    document.getElementById("titulo").innerHTML = "Hola, " + cliente + " Tu saldo es";
+    document.getElementById("descripcion").innerHTML = " $ " + saldo;
     
 }
 
@@ -65,11 +80,14 @@ let retiro = parseInt(document.querySelector("#retirar").value);
 let indice = parseInt(document.querySelector("#cuenta").value);
 let saldo = cuenta [indice-1].saldo;
 
-console.log(saldo);
-
 document.getElementById("descripcion").innerHTML =saldo-retiro;
 cuenta [indice-1].saldo = saldo-retiro;
 
+if( saldo < 11 ) {
+    document.querySelector("#descripcion").innerHTML="Saldo insuficiente"
+} else (document.querySelector("#bienvenido").innerHTML="Transaccion Exitosa")
+
+console.log(saldo);
 }
 
 /*CONSIGNAR PERSONA*/
@@ -79,12 +97,13 @@ function leerconsignar(){
     let indice = parseInt(document.querySelector("#cuenta").value);
     let saldo = cuenta [indice-1].saldo;
     
-    console.log(saldo);
-    
     document.getElementById("descripcion").innerHTML = saldo+consignar;
     cuenta [indice-1].saldo = saldo+consignar;
-   
 
+    if( saldo >990) {
+        document.getElementById("descripcion").alert="Excedio su saldo, consuelte a un asesor bancario" }
+        else { (document.getElementById("bienvenido").innerHTML="Transaccion Exitosa")
     }
-
+    console.log(saldo);
+}
 
